a9f112c89d21d146aaa6bcae3f6783575578deb5
KSadziute <kristina.sadziute@gmail.com>
2023-02-23 16:05:08 +0200

    commit2

---
+++ swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+  
+    
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    
+})  
