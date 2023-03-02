b5255e80886f2a41fce7781f4d74f0734088ab79
KSadziute <kristina.sadziute@gmail.com>
2023-03-02 14:47:22 +0200

    commit5

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function showTab(name){
 document.getElementById(name).style.display = "block"
 }
 showTab("pirmas")
+
+
+const burger = document.getElementById("burger")
+const links = document.querySelector(".links")
+
+burger.addEventListener("click",function(){
+    this.classList.toggle("rotate")
+links.classList.toggle("open")
+})
